3c4a5517a888dab836bfb9304a01193ac28f0152
aure.martini <aure.martini@gmail.com>
2024-02-15 21:53:41 +0100

    Setup Moodle preinstall

--- moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'TKBPuNDjdvSzVAcaAMDLBLa3EWi2tEfYlocalhost';
-$CFG->bootstraphash = '5e45cba75aa264da44d681dacdf1a711';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
