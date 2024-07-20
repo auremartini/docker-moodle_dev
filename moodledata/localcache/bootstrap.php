b25e704d1dce0561b0c4a15187339e2075c91731
Aureliano Martini <aure.martini@gmail.com>
2024-07-20 15:06:09 +0300

    Purge moodledata/localcache

--- moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'yRrKsXUzzVSmPJb7Hgd1fuRoAUkLDPSLlocalhost';
-$CFG->bootstraphash = 'c619a5f452c5f70858195c6050bd2698';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
